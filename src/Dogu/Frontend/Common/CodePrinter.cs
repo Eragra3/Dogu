41d4c5304e9166b47172a7eaf231e2bc84d9adf1
Daniel Bider <bider.daniel@gmail.com>
2020-02-11 01:01:12 +0100

    Simplified basic C# types and nullable types

--- src/Dogu/Frontend/Common/CodePrinter.cs
+++ src/Dogu/Frontend/Common/CodePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Dogu.Backend;
 
 namespace Dogu.Frontend.Common
@@ -21,28 +22,62 @@ namespace Dogu.Frontend.Common
             };
         }
 
+        public static System.Nullable<Kek<System.Text.Json.Serialization.JsonConverter<System.Nullable<System.Int32>>>> kek  => null;
+        public static Kek<System.Text.Json.Serialization.JsonConverter<System.Int32?>>?                                 kek2 => null;
+
         public static string SimplifyLibraryTypes(string fullTypeName)
         {
-            return fullTypeName switch
-            {
-                "System.Object"  => "object",
-                "System.String"  => "string",
-                "System.Char"    => "char",
-                "System.Decimal" => "decimal",
-                "System.Double"  => "double",
-                "System.Single"  => "float",
-                "System.UInt64"  => "ulong",
-                "System.UInt32"  => "uint",
-                "System.UInt16"  => "ushort",
-                "System.Int64"   => "long",
-                "System.Int32"   => "int",
-                "System.Int16"   => "short",
-                "System.Byte"    => "byte",
-                "System.SByte"   => "sbyte",
-                "System.Boolean" => "bool",
-                "System.Void"    => "void",
-                _                => fullTypeName
-            };
+            // Change `System.Nullable<>` into `?`
+            string simplifiedType = ReplaceNullableWithQuestionMark(fullTypeName);
+
+            simplifiedType = simplifiedType
+                .Replace("System.Object", "object")
+                .Replace("System.String", "string")
+                .Replace("System.Char", "char")
+                .Replace("System.Decimal", "decimal")
+                .Replace("System.Double", "double")
+                .Replace("System.Single", "float")
+                .Replace("System.UInt64", "ulong")
+                .Replace("System.UInt32", "uint")
+                .Replace("System.UInt16", "ushort")
+                .Replace("System.Int64", "long")
+                .Replace("System.Int32", "int")
+                .Replace("System.Int16", "short")
+                .Replace("System.Byte", "byte")
+                .Replace("System.SByte", "sbyte")
+                .Replace("System.Boolean", "bool")
+                .Replace("System.Void", "void");
+
+            return simplifiedType;
         }
+
+        public static string ReplaceNullableWithQuestionMark(string fullTypeName)
+        {
+            const string nullableLeftPart  = "System.Nullable<";
+            const string nullableRightPart = ">";
+
+            string result = fullTypeName;
+
+            int nullableTypeIndex = fullTypeName.IndexOf(nullableLeftPart);
+
+            while (nullableTypeIndex > -1)
+            {
+                int rightAngleBracketIndex = fullTypeName.IndexOf(nullableRightPart, nullableTypeIndex);
+                int innerTypeIndex         = nullableTypeIndex + nullableLeftPart.Length;
+
+                result = fullTypeName.Substring(0, nullableTypeIndex)
+                         + fullTypeName.Substring(innerTypeIndex, rightAngleBracketIndex - innerTypeIndex)
+                         + "?"
+                         + fullTypeName.Substring(rightAngleBracketIndex + 1, fullTypeName.Length - rightAngleBracketIndex - 1);
+
+                nullableTypeIndex = fullTypeName.IndexOf(result);
+            }
+
+            return result;
+        }
+    }
+
+    public struct Kek<T>
+    {
     }
 }
